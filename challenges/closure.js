a8ff75c220ed1a07969a94ccf83de5c6d727b421
travis-mcminimy <tmcminimy@gmail.com>
2020-10-09 19:57:01 -0700

    I hope I didn't forget anything

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,17 @@ myFunction();
 
 // Explanation: 
 
+/* Because it is located within the primary function's scope, and it does not have to 'leave its territory' in order to use it.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(number){
+  var sum = 0;
+  for(var i = 1; i <= number; i++){
+    sum += i;
+  }
+  return sum;
+}
+console.log(summation(4));
